661837bbfbd8f39f890452c4de1ed68259ff3bb6
Mario-1996 <mario1996prohman@gmail.com>
2022-08-29 22:24:09 -0300

    Docker

---
+++ src/app.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { join } from 'path';
+import { ScheduleModule } from '@nestjs/schedule';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TestController } from './test/test.controller';
+import { TestService } from './test/test.service';
+import { TweetsModule } from './tweets/tweets.module';
+
+@Module({
+  imports: [
+    ScheduleModule.forRoot(),
+    SequelizeModule.forRoot({
+      dialect: 'sqlite',
+      host: join(__dirname, 'database.sqlite'),
+      autoLoadModels: true,
+      synchronize: true,
+    }),
+    TweetsModule,
+  ],
+  controllers: [AppController, TestController],
+  providers: [AppService, TestService],
+})
+export class AppModule {}
